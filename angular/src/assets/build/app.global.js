c429d5ed2e791e31e2f3c9cfb451a5b0af985f39
Severin Friede <sevefriede@gmail.com>
2018-01-25 10:47:26 +0100

    updated to latest versions of angular and stencil and added polyfills for IE11

--- angular/src/assets/build/app.global.js
+++
@@ -1,4 +0,0 @@
-/*! Built with http://stenciljs.com */
-(function(appNamespace,publicPath){"use strict";
-!function(n){var t=Object.assign||function(n){for(var t,r=1,e=arguments.length;r<e;r++){t=arguments[r];for(var o in t)Object.prototype.hasOwnProperty.call(t,o)&&(n[o]=t[o])}return n};Context.activeRouter=function(){function n(){for(var n=e,t=0;t<n.length;t++)(0,n[t])()}var r={},e=[];return{set:function(e){r=t({},r,e),n()},get:function(n){return n?r[n]:r},subscribe:function(n){if("function"!=typeof n)throw new Error("Expected listener to be a function.");var t=!0;return e.push(n),function(){if(t){t=!1;var r=e.indexOf(n);e.splice(r,1)}}}}}()}(publicPath);
-})("App","/assets/build/app/");
---
+++ angular/src/assets/build/app/app.global.js
@@ -0,0 +1,5 @@
+/*! Built with http://stenciljs.com */
+(function(appNamespace,publicPath){"use strict";
+(function(publicPath){Context.activeRouter=function(){var n={},t={},o={},e=[];return{set:function(t){n=Object.assign({},n,t),o={},function(){for(var n=e,t=0;t<n.length;t++){var o=n[t];o()}}()},get:function(t){return 0===Object.keys(n).length?{location:{pathname:Context.window.location.pathname,search:Context.window.location.search}}:t?n[t]:n},subscribe:function(n){if("function"!=typeof n)throw new Error("Expected listener to be a function.");var t=!0;return e.push(n),function(){if(t){t=!1;var o=e.indexOf(n);e.splice(o,1)}}},addToGroup:function(n,o){o in t||(t[o]=[]),t[o].push(n)},removeFromGroups:function(n){for(var o in t){var e=t[o];t[o]=e.filter(function(t){return t!==n})}},didGroupAlreadyMatch:function(n){return!!n&&!0===o[n]},setGroupMatched:function(n){o[n]=!0}}}();
+})(publicPath);
+})("App","/assets/build/app/");
